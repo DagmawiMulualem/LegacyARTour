cd5044aaf47c2641d8e839a7e957d08f071e531c
Dagmawi Mulualem <Dagmawi.m.mulualem@gmail.com>
2025-05-31 01:48:15 -0400

    Initial Commit

---
+++ LegacyARTourTests/LegacyARTourTests.swift
@@ -0,0 +1,17 @@
+//
+//  LegacyARTourTests.swift
+//  LegacyARTourTests
+//
+//  Created by Dagmawi Mulualem on 5/31/25.
+//
+
+import Testing
+@testable import LegacyARTour
+
+struct LegacyARTourTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
